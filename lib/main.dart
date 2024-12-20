0c87096af30631fc48e6af0136c384db556ae54b
ny109 <nischal22bca109@ncuindia.edu>
2024-12-20 14:26:34 +0000

    initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,115 +1,297 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(ShoppingListApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class ShoppingListApp extends StatelessWidget {
+  const ShoppingListApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Shopping List App',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: HomeScreen(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _HomeScreenState createState() => _HomeScreenState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _HomeScreenState extends State<HomeScreen> {
+  List<Map<String, String>> shoppingList = [];
+  String selectedFilter = "All"; // Default filter
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+  final List<String> categories = [
+    "All",
+    "Dairy",
+    "Fruits",
+    "Vegetables",
+    "Beverages",
+    "Personal Care",
+    "Others",
+  ];
+
+  final Map<String, IconData> categoryIcons = {
+  "Dairy": Icons.local_grocery_store,
+  "Fruits": Icons.apple_outlined,
+  "Vegetables": Icons.eco,
+  "Beverages": Icons.coffee,
+  "Personal Care": Icons.clean_hands_outlined,
+  "Others": Icons.interests,
+};
+
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    // Filter shopping list based on selected category
+    List<Map<String, String>> filteredList = selectedFilter == "All"
+        ? shoppingList
+        : shoppingList
+            .where((item) => item['category'] == selectedFilter)
+            .toList();
+
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text('Shopping List'),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.add),
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => AddItemScreen(
+                    onAddItem: (item) {
+                      setState(() {
+                        shoppingList.add(item);
+                      });
+                    },
+                    categories: categories.sublist(1), // Exclude "All"
+                  ),
+                ),
+              );
+            },
+          ),
+        ],
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+      body: Column(
+        children: [
+          // Dropdown for category filter
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: DropdownButton<String>(
+              value: selectedFilter,
+              items: categories.map((String category) {
+                return DropdownMenuItem<String>(
+                  value: category,
+                  child: Text(category),
+                );
+              }).toList(),
+              onChanged: (value) {
+                setState(() {
+                  selectedFilter = value!;
+                });
+              },
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+          ),
+          // Display filtered shopping list
+          Expanded(
+            child: filteredList.isEmpty
+                ? Center(child: Text('No items in this category'))
+                : ListView.builder(
+                    itemCount: filteredList.length,
+                    itemBuilder: (context, index) {
+                      return ListTile(
+                        leading: Icon(
+                          categoryIcons[filteredList[index]['category']] ??
+                              Icons.category, // Default icon
+                          color: Colors.blue,
+                        ),
+                        title: Text(filteredList[index]['name']!),
+                        subtitle: Text(
+                          'Qty: ${filteredList[index]['quantity']} - Category: ${filteredList[index]['category']}',
+                        ),
+                        trailing: Row(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            IconButton(
+                              icon: Icon(Icons.edit, color: Colors.orange),
+                              onPressed: () {
+                                Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                    builder: (context) => EditItemScreen(
+                                      item: filteredList[index],
+                                      onEditItem: (updatedItem) {
+                                        setState(() {
+                                          int originalIndex =
+                                              shoppingList.indexWhere((item) =>
+                                                  item == filteredList[index]);
+                                          shoppingList[originalIndex] =
+                                              updatedItem;
+                                        });
+                                      },
+                                    ),
+                                  ),
+                                );
+                              },
+                            ),
+                            IconButton(
+                              icon: Icon(Icons.delete, color: Colors.red),
+                              onPressed: () {
+                                setState(() {
+                                  shoppingList.remove(filteredList[index]);
+                                });
+                              },
+                            ),
+                          ],
+                        ),
+                      );
+                    },
+                  ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class AddItemScreen extends StatelessWidget {
+  final Function(Map<String, String>) onAddItem;
+  final List<String> categories;
+
+  AddItemScreen({super.key, required this.onAddItem, required this.categories});
+
+  final TextEditingController nameController = TextEditingController();
+  final TextEditingController quantityController = TextEditingController();
+  String selectedCategory = "Dairy"; // Default category
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Add Item'),
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            TextField(
+              controller: nameController,
+              decoration: InputDecoration(labelText: 'Item Name'),
+            ),
+            TextField(
+              controller: quantityController,
+              decoration: InputDecoration(labelText: 'Quantity'),
+              keyboardType: TextInputType.number,
+            ),
+            DropdownButton<String>(
+              value: selectedCategory,
+              items: categories.map((String category) {
+                return DropdownMenuItem<String>(
+                  value: category,
+                  child: Text(category),
+                );
+              }).toList(),
+              onChanged: (value) {
+                selectedCategory = value!;
+              },
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                onAddItem({
+                  'name': nameController.text,
+                  'quantity': quantityController.text,
+                  'category': selectedCategory,
+                });
+                Navigator.pop(context);
+              },
+              child: Text('Add Item'),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+class EditItemScreen extends StatelessWidget {
+  final Map<String, String> item;
+  final Function(Map<String, String>) onEditItem;
+
+  EditItemScreen({super.key, required this.item, required this.onEditItem});
+
+  final TextEditingController nameController = TextEditingController();
+  final TextEditingController quantityController = TextEditingController();
+  late String selectedCategory;
+
+  @override
+  Widget build(BuildContext context) {
+    nameController.text = item['name']!;
+    quantityController.text = item['quantity']!;
+    selectedCategory = item['category']!;
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Edit Item'),
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            TextField(
+              controller: nameController,
+              decoration: InputDecoration(labelText: 'Item Name'),
+            ),
+            TextField(
+              controller: quantityController,
+              decoration: InputDecoration(labelText: 'Quantity'),
+              keyboardType: TextInputType.number,
+            ),
+            DropdownButton<String>(
+              value: selectedCategory,
+              items: [
+                "Dairy",
+                "Fruits",
+                "Vegetables",
+                "Beverages",
+                "Personal Care",
+                "Others"
+              ].map((String category) {
+                return DropdownMenuItem<String>(
+                  value: category,
+                  child: Text(category),
+                );
+              }).toList(),
+              onChanged: (value) {
+                selectedCategory = value!;
+              },
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                onEditItem({
+                  'name': nameController.text,
+                  'quantity': quantityController.text,
+                  'category': selectedCategory,
+                });
+                Navigator.pop(context);
+              },
+              child: Text('Save Changes'),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
